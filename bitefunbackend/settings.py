3425892950e0a79f3843ea1b9a9e1250359a6858
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-12-08 05:18:39 +0300

    change some part

--- bitefunbackend/settings.py
+++ bitefunbackend/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'info.neyman.e.t@gmail.com'
-EMAIL_HOST_PASSWORD = 'Neyman123'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'examplepassword'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Flutter geliştirme ortamı
